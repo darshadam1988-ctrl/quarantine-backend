3c54d4f7541e4ba0eb62074ee43cc8b7438a4035
darshadam1988-ctrl <darshadam1988@gmail.com>
2026-05-25 02:36:56 +0300

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,25 @@
 const admin = require("firebase-admin");
 
-// نقرأ البيانات من المتغير السري الذي وضعناه في Render
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+// هذا الكود يقرأ الإعدادات من "متغيرات البيئة" (Environment Variables)
+// التي سنضعها في لوحة تحكم Railway أو أي موقع استضافة
+const serviceAccount = {
+  "type": "service_account",
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+  // نقوم باستبدال الرموز الخاصة بالأسطر الجديدة التي قد تفسد التنسيق
+  "private_key": process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  "client_id": process.env.FIREBASE_CLIENT_ID,
+  "auth_uri": process.env.FIREBASE_AUTH_URI,
+  "token_uri": process.env.FIREBASE_TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
+
 module.exports = db;
