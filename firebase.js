f9bafa53bceee8b92c3fbf80b2340a8a2a2b4fca
darshadam1988-ctrl <darshadam1988@gmail.com>
2026-05-25 02:25:16 +0300

    aaddaamm

--- firebase.js
+++ firebase.js
@@ -1,8 +1,11 @@
 const admin = require("firebase-admin");
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG_JSON); // سيقرأ البيانات من إعدادات موقع Render
+
+// نقرأ البيانات من المتغير السري الذي وضعناه في Render
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-module.exports = admin.firestore();
+const db = admin.firestore();
+module.exports = db;
