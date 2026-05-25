51addcca1cfafa8793e90b72f47f0d0290995b3e
darshadam1988-ctrl <darshadam1988@gmail.com>
2026-05-25 03:34:15 +0300

    aaadddaaammm

--- firebase.js
+++ firebase.js
@@ -8,7 +8,7 @@ if (!process.env.FIREBASE_PROJECT_ID || !process.env.FIREBASE_PRIVATE_KEY) {
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    project_id: process.env.FIREBASE_PROJECT_ID,
+    project_id: process.env.FIREBASE_PROJECT_ID(/\\n/g, '\n'),
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
     private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
