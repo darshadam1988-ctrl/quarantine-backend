078795eae3c35468a249f068d9fbcafe54272936
darshadam1988-ctrl <darshadam1988@gmail.com>
2026-05-25 03:39:03 +0300

    aaadddaaammm

--- firebase.js
+++ firebase.js
@@ -1,11 +1,15 @@
 const admin = require("firebase-admin");
 
+// تنظيف المفتاح من أي تداخلات في Railway
+const privateKey = process.env.FIREBASE_PRIVATE_KEY 
+  ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n').replace(/"/g, '') 
+  : '';
+
 admin.initializeApp({
   credential: admin.credential.cert({
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    // السطر المهم هنا هو التالي، تأكد أنه يخص الـ private_key فقط
-    private_key: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : '',
+    private_key: privateKey, // المفتاح المنظف
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: process.env.FIREBASE_AUTH_URI,
@@ -15,5 +19,4 @@ admin.initializeApp({
   })
 });
 
-const db = admin.firestore();
-module.exports = db;
+module.exports = admin.firestore();
